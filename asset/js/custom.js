0b047487c94c09c42c92c1fd9633d0c86c9ff8b6
ohhyerim <tmvkfmxk00@naver.com>
2022-09-05 01:09:38 +0900

    #weekday css

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,7 +1,6 @@
 $(function(){
     // swiper slide 
     const swiper1 = new Swiper(".slide1 .swiper", {
-        // speed:1000,
         slidesPerView: "auto",
         spaceBetween: 0,
       });
@@ -10,21 +9,29 @@ $(function(){
         spaceBetween: 8,
         loop: true,
         pagination: {
-            el: ".fraction",
+            el: ".fraction1",
             type: "fraction",
           }
       });
-      slide3Arr = ['10대여자','10대남자','20대여자','20대남자','30대여자','30대남자']
-      const swiper3 = new Swiper(".slide3 .swiper", {
-        // speed:1000,
-        slidesPerView: "auto",
-        spaceBetween: 0,
-        pagination: {
-          el: ".sc-age .tab-area",
-          clickable: true,
-          renderBullet: function (index, className) {
-              return `<a href="#" class="btn-tab ${className}">${slide3Arr[index]}</a>`;
-          }
-      }
+    slide3Arr = ['10대여자','10대남자','20대여자','20대남자','30대여자','30대남자']
+    const swiper3 = new Swiper(".slide3 .swiper", {
+      slidesPerView: "auto",
+      spaceBetween: 0,
+      pagination: {
+        el: ".sc-age .tab-area",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return `<a href="#" class="btn-tab ${className}">${slide3Arr[index]}</a>`;
+         }
+        }
       });
+    const swiper4 = new Swiper(".slide4 .swiper", {
+      slidesPerView: "auto",
+      spaceBetween: 0,
+      loop:true,
+      pagination: {
+        el: ".fraction2",
+        type: "fraction",
+      }
+    });
 })
